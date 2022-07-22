aa6e32cfef2a177ed323efc4fd0669bb77c8073f
Anuradha Biswas <109795091+anuradhab1@users.noreply.github.com>
2022-07-22 15:31:04 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3 and Blueberry Oatmeal")
 streamlit.text("Kale, Spinach and Rocket Smoothey")
 
-streamit.text("harry potter")
+streamlit.text("harry potter")
