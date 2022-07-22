950e0a7bf753b3efcab48c99ee73550e320cdb4d
Anuradha Biswas <109795091+anuradhab1@users.noreply.github.com>
2022-07-22 15:32:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3 and Blueberry Oatmeal")
 streamlit.text("Kale, Spinach and Rocket Smoothey")
 
-streamlit.text("harry potter")
+streamlit.text('Hard-Boiled Free-Range Egg')
