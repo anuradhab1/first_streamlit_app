f2574304ce1da779afcb9fa3b3c5015e63826b35
Anuradha Biswas <109795091+anuradhab1@users.noreply.github.com>
2022-07-22 15:30:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.title("My Parents Ner Healthy Dinner")
 streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3 and Blueberry Oatmeal")
 streamlit.text("Kale, Spinach and Rocket Smoothey")
+
+streamit.text("harry potter")
